0cd4a3547b56cb256a70be151a10eebeee013b6b
Pramit Barua <pramit.barua@appsfactory.de>
2022-08-29 18:14:10 +0200

    introduced typescript

--- Backend/routes/categoryRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const categoryControllers = require('./../controllers/categoryControllers')
-
-const router = express.Router();
-
-router.route('/').get(categoryControllers.getAllCategory).post(categoryControllers.createCategory).delete(categoryControllers.deleteCategory);
-
-module.exports = router;
